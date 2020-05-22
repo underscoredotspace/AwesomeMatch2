bb9af4f562da0dfcb45a8c69d576fbf927e6a73b
Colin Tindle <colin.tindle@fanduel.com>
2020-05-22 17:51:02 +0100

    initial setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { SafeAreaView } from "react-native"
+import Main from "./src/index"
+
+const App = () => (
+    <SafeAreaView>
+        <Main />
+    </SafeAreaView>
+)
+
+export default App
