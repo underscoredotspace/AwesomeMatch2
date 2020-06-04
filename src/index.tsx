d7ac480e7d417425fce583318b85796e0ba89538
Colin Tindle <colin.tindle@fanduel.com>
2020-06-04 17:46:01 +0100

    starter tiles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,51 @@
 import React from "react"
 import { View, Text } from "./primitives"
+import { useWindowDimensions } from "react-native"
 
-export default () => (
-    <View>
-        <Text style={{ fontSize: 20 }}>
-            This is some text in a really nice font
-        </Text>
-    </View>
-)
+export default () => {
+    const { width } = useWindowDimensions()
+    const rows = [
+        ["a", "b", "c", "d"],
+        ["e", "f", "g", "h"],
+        ["i", "j", "k", "l"],
+        ["m", "n", "o", "p"],
+    ]
+    return (
+        <>
+            <View>
+                <Text style={{ fontSize: 20 }}>
+                    This is some text in a really nice font
+                </Text>
+            </View>
+            <View style={{ height: width }}>
+                {rows.map((row, rowNdx) => (
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            width,
+                            flex: 1,
+                            paddingVertical: 2,
+                        }}
+                        key={`row-${rowNdx}`}
+                    >
+                        {row.map((tile, tileNdx) => (
+                            <View
+                                key={`tile-${rowNdx}-${tileNdx}`}
+                                style={{
+                                    flex: 1,
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderColor: "black",
+                                    borderWidth: 2,
+                                    marginHorizontal: 2,
+                                }}
+                            >
+                                <Text>{tile}</Text>
+                            </View>
+                        ))}
+                    </View>
+                ))}
+            </View>
+        </>
+    )
+}
